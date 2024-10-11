5e802312bf03aa07a57c34f14cb7fc46eca0adb1
Wang-Phil <1420767429@qq.com>
2024-10-11 16:13:52 +0800

    查询用户信息实现

---
+++ admin/src/main/java/org/offer/shortlink/admin/ShortLinkAdminApplication.java
@@ -0,0 +1,13 @@
+package org.offer.shortlink.admin;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("org.offer.shortlink.admin.dao.mapper")
+public class ShortLinkAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkAdminApplication.class, args);
+    }
+}
---
+++ admin/src/main/java/org/offer/shortlink/admin/dao/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package org.offer.shortlink.admin.dao.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.offer.shortlink.admin.dao.entity.UserDO;
+
+/**
+ * 用户持久层
+ */
+public interface UserMapper extends BaseMapper<UserDO> {
+
+}
