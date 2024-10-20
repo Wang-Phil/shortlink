489658e098a50e53c72a260a7a75cb1407607304
Wang-Phil <1420767429@qq.com>
2024-10-20 22:42:50 +0800

    feature: 短链接后管系统调用中台请求相关接口

--- admin/src/main/java/org/offer/shortlink/admin/remote/dto/req/ShortLinkPageReqDTO.java
+++ admin/src/main/java/org/offer/shortlink/admin/remote/dto/req/ShortLinkPageReqDTO.java
@@ -2,13 +2,12 @@ package org.offer.shortlink.admin.remote.dto.req;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.Data;
-import org.offer.shortlink.project.dao.entity.ShortLinkDO;
 
 /**
  * 短链接分页请求参数
  */
 @Data
-public class ShortLinkPageReqDTO extends Page<ShortLinkDO> {
+public class ShortLinkPageReqDTO extends Page {
 
     /**
      * 分组标识
