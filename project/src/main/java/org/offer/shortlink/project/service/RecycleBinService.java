a27509a947d372b4d6a3799c3f4789d9bd4fbe98
Wang-Phil <1420767429@qq.com>
2024-10-22 21:43:05 +0800

    feature: 开发短链接新增回收站功能

---
+++ project/src/main/java/org/offer/shortlink/project/service/RecycleBinService.java
@@ -0,0 +1,18 @@
+package org.offer.shortlink.project.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import org.offer.shortlink.project.dao.entity.ShortLinkDO;
+import org.offer.shortlink.project.dto.req.RecycleBinSaveReqDTO;
+
+/**
+ * 回收站管理接口层
+ */
+public interface RecycleBinService extends IService<ShortLinkDO> {
+
+    /**
+     * 保存回收站
+     *
+     * @param requestParam 请求参数
+     */
+    void saveRecycleBin(RecycleBinSaveReqDTO requestParam);
+}
---
+++ project/src/main/java/org/offer/shortlink/project/service/impl/RecycleBinServiceImpl.java
@@ -0,0 +1,39 @@
+package org.offer.shortlink.project.service.impl;
+
+
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.offer.shortlink.project.dao.entity.ShortLinkDO;
+import org.offer.shortlink.project.dao.mapper.ShortLinkMapper;
+import org.offer.shortlink.project.dto.req.RecycleBinSaveReqDTO;
+import org.offer.shortlink.project.service.RecycleBinService;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+import static org.offer.shortlink.project.common.constant.RedisKeyConstant.GOTO_SHORT_LINK_KEY;
+
+/**
+ * 回收站管理接口实现层
+ */
+@Service
+@RequiredArgsConstructor
+public class RecycleBinServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLinkDO> implements RecycleBinService {
+
+    private final StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public void saveRecycleBin(RecycleBinSaveReqDTO requestParam) {
+        LambdaUpdateWrapper<ShortLinkDO> updateWrapper = Wrappers.lambdaUpdate(ShortLinkDO.class)
+                .eq(ShortLinkDO::getFullShortUrl, requestParam.getFullShortUrl())
+                .eq(ShortLinkDO::getGid, requestParam.getGid())
+                .eq(ShortLinkDO::getEnableStatus, 0)
+                .eq(ShortLinkDO::getDelFlag, 0);
+        ShortLinkDO shortLinkDO = ShortLinkDO.builder()
+                .enableStatus(1)
+                .build();
+        baseMapper.update(shortLinkDO, updateWrapper);
+        stringRedisTemplate.delete(String.format(GOTO_SHORT_LINK_KEY, requestParam.getFullShortUrl()));
+    }
+}
