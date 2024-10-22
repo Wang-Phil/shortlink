16d2681145d0ccd41e2b3ab24a9adad28cc14a43
Wang-Phil <1420767429@qq.com>
2024-10-22 21:43:15 +0800

    feature: 开发短链接新增回收站功能

---
+++ admin/src/main/java/org/offer/shortlink/admin/controller/RecycleBinController.java
@@ -0,0 +1,34 @@
+package org.offer.shortlink.admin.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.offer.shortlink.admin.common.convention.result.Result;
+import org.offer.shortlink.admin.common.convention.result.Results;
+import org.offer.shortlink.admin.dto.req.RecycleBinSaveReqDTO;
+import org.offer.shortlink.admin.remote.ShortLinkRemoteService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 回收站管理控制层
+ */
+@RestController
+@RequiredArgsConstructor
+public class RecycleBinController {
+
+    /**
+     * 后续重构为 SpringCloud Feign 调用
+     */
+    ShortLinkRemoteService shortLinkRemoteService = new ShortLinkRemoteService() {
+    };
+
+    /**
+     * 保存回收站
+     */
+    @PostMapping("/api/short-link/v1/recycle-bin/save")
+    public Result<Void> saveRecycleBin(@RequestBody RecycleBinSaveReqDTO requestParam) {
+        shortLinkRemoteService.saveRecycleBin(requestParam);
+        return Results.success();
+    }
+}
