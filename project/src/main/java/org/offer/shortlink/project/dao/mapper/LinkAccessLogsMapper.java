60fda330435a1f510ce310a0fdb311441d82af1f
Wang-Phil <1420767429@qq.com>
2024-10-23 15:26:22 +0800

    feature: 开发短链接监控之高频访问IP功能

---
+++ project/src/main/java/org/offer/shortlink/project/dao/mapper/LinkAccessLogsMapper.java
@@ -0,0 +1,10 @@
+package org.offer.shortlink.project.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.offer.shortlink.project.dao.entity.LinkAccessLogsDO;
+
+/**
+ * 访问日志监控持久层
+ */
+public interface LinkAccessLogsMapper extends BaseMapper<LinkAccessLogsDO> {
+}
