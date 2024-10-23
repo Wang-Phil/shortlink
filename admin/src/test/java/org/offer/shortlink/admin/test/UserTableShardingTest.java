bbf00b504d3692cb88045b84a682a6720ded3f5a
Wang-Phil <1420767429@qq.com>
2024-10-23 21:03:57 +0800

    feature: 短链接新增时初始化访问统计为0

--- admin/src/test/java/org/offer/shortlink/admin/test/UserTableShardingTest.java
+++ admin/src/test/java/org/offer/shortlink/admin/test/UserTableShardingTest.java
@@ -18,9 +18,20 @@
 package org.offer.shortlink.admin.test;
 public class UserTableShardingTest {
 
-    public static final String SQL = "ALTER TABLE t_link_%d ADD COLUMN total_uv INT DEFAULT 0 COMMENT '历史uv' after `describe` , \n" +
-            "ADD COLUMN total_pv INT DEFAULT 0 COMMENT '历史pv' after total_uv , \n" +
-            "ADD COLUMN total_uip INT DEFAULT 0 COMMENT '历史uip' after total_pv;";
+    public static final String SQL = "CREATE TABLE `t_link_stats_today_%d` ( +\n" +
+            "          `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID', +\n" +
+            "          `gid` varchar(32) DEFAULT 'default' COMMENT '分组标识', +\n" +
+            "          `full_short_url` varchar(128) DEFAULT NULL COMMENT '短链接', +\n" +
+            "          `date` date DEFAULT NULL COMMENT '日期', +\n" +
+            "          `today_pv` int(11) DEFAULT '0' COMMENT '今日PV', +\n" +
+            "          `today_uv` int(11) DEFAULT '0' COMMENT '今日UV', +\n" +
+            "          `today_ip_count` int(11) DEFAULT '0' COMMENT '今日IP数', +\n" +
+            "          `create_time` datetime DEFAULT NULL COMMENT '创建时间', +\n" +
+            "          `update_time` datetime DEFAULT NULL COMMENT '修改时间', +\n" +
+            "          `del_flag` tinyint(1) DEFAULT NULL COMMENT '删除标识 0：未删除 1：已删除', +\n" +
+            "          PRIMARY KEY (`id`), +\n" +
+            "          UNIQUE KEY `idx_unique_today_stats` (`full_short_url`,`gid`,`date`) USING BTREE +\n" +
+            "        ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;;";
 
     public static void main(String[] args) {
         for (int i = 0; i < 16; i++) {
